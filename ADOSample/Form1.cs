9ea4f8ed9bd87a201eac0d51a3f7b7f8cc34cefb
Aimimashio <31083@ojs.ac.jp>
2020-07-14 14:00:44 +0900

    データベースと接続

--- ADOSample/Form1.cs
+++ ADOSample/Form1.cs
@@ -16,5 +16,23 @@ namespace ADOSample
         {
             InitializeComponent();
         }
+
+        private void memoTableBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.memoTableBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.infosys202008DataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.memoTableTableAdapter.Fill(this.infosys202008DataSet.memoTable);
+        }
     }
 }
